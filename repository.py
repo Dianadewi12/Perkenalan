3ea796f4072d2a608a8a632ae36f257154ddf1a0
Dianadewi12 <94312504+Dianadewi12@users.noreply.github.com>
2021-11-16 18:27:54 +0700

    Add files via upload

---
+++ repository.py
@@ -0,0 +1,7 @@
+nama = 'Titik Tri Diana Dewi'
+NIM = 212410102057
+fakultas = 'Fakultas Ilmu Komputer'
+prodi = 'Teknologi Informasi'
+kelompok = '28'
+asal = 'Jember'
+print(f'Hallo perkenalkan, nama Saya {nama} dengan NIM {NIM}, Saya merupakan mahasiswa Universitas Jember {fakultas} prodi {prodi}, dari kelompok {kelompok}, Saya berasal dari {asal}')
